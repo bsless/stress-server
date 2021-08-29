dbf46923656bf21462a2f2a264cc9c8debfc0877
ben.sless <ben.sless@appsflyer.com>
2021-08-29 17:30:06 +0300

    Add async arities to ring handlers

--- src/com/github/bsless/stress_server/ring_middleware.clj
+++ src/com/github/bsless/stress_server/ring_middleware.clj
@@ -34,15 +34,25 @@
          {:get {:summary "plus with malli query parameters"
                 :parameters {:query [:map [:x int?] [:y int?]]}
                 :responses {200 {:body [:map [:total int?]]}}
-                :handler (fn [{{{:keys [x y]} :query} :parameters}]
-                           {:status 200
-                            :body {:total (+ x y)}})}
+                :handler (fn
+                           ([{{{:keys [x y]} :query} :parameters}]
+                            {:status 200
+                             :body {:total (+ x y)}})
+                           ([{{{:keys [x y]} :query} :parameters} respond _raise]
+                            (respond
+                             {:status 200
+                              :body {:total (+ x y)}})))}
           :post {:summary "plus with malli body parameters"
                  :parameters {:body [:map [:x int?] [:y int?]]}
                  :responses {200 {:body [:map [:total int?]]}}
-                 :handler (fn [{{{:keys [x y]} :body} :parameters}]
-                            {:status 200
-                             :body {:total (+ x y)}})}}]]]
+                 :handler (fn
+                            ([{{{:keys [x y]} :body} :parameters}]
+                             {:status 200
+                              :body {:total (+ x y)}})
+                            ([{{{:keys [x y]} :body} :parameters} respond _raise]
+                             (respond
+                              {:status 200
+                               :body {:total (+ x y)}})))}}]]]
 
       {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
        ;;:validate spec/validate ;; enable spec validation for route data
